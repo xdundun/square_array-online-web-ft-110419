da984956be661011535382241f070c47728f6d2b
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 18:39:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  square_array = []
+  counter = 0
+  array.each do |square|
+    puts "#{square}"
+     square_array[counter] = square**2
+     counter +=1
+end
+return square_array
 end
